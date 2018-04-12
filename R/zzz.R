c66a32de96a8a576892f3dc3660d165aa8f99eec
Nima Hejazi <nh@nimahejazi.org>
2018-04-12 13:55:26 -0700

    fix remotes

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
   packageStartupMessage(paste0(
     "hal9001 v",
     utils::packageDescription("hal9001")$Version,
-    ": The scalable Highly Adaptive Lasso"
+    ": The Scalable and Efficient Highly Adaptive Lasso"
   ))
 }
