66bb6bdc4dda4eb52f2efc1492f63d32843c3264
Lars van der Laan <larsvanderlaan@berkeley.edu>
2020-07-30 10:47:30 -0700

    Added : and * functionality to formula

--- R/formula_hal9001.R
+++ R/formula_hal9001.R
@@ -31,12 +31,15 @@
 #' Note it should be of length 1 or length ncol(data)-1. Vector recycling will be employed otherwise.
 #' @param include_zero_order Same as \code{include_zero_order} in function \code{fit_hal}
 #' @param bins Same as \code{bins} in function \code{fit_hal}
+#' @param generate_lower_degrees Boolean indicator for whether all lower interaction/main term basis functions should be generated for each
+#' specified term. If true then "y~h(x,w)" behaves similar to "y~x*w" in \code{formula},
+#' and if false then it behaves similar to "y~x:y" in \code{formula}.
 #' @param exclusive_dot Boolean indicator for whether the "." and ".^max_degree" arguments in the formula
 #' should be treated as exclusive or inclusive the variables already specified in the formula.
 #' For example, if "y ~ h(x,w) + ." should the "." be interpreted as: add all one-way basis functions
 #' for the variables remaining in \code{data} not yet specified in the formula (i.e. excluding x,w),
 #' or: add all one-way basis functions for all variables in the model (including x,w).
-#' As an example. if \code{exclusive_dot} is false then "y ~h(x) + .^2" and "y ~ .^2" specify the same formula, i.e. generate all basis functions up to degree 2.
+#' As an example. if \code{exclusive_dot} is false then "y ~ h(x) + .^2" and "y ~ .^2" specify the same formula, i.e. generate all basis functions up to degree 2.
 #' However, if \code{exclusive_dot} is true, then "y ~ h(x) + .^2"  encodes a different formula than "y ~ .^2".
 #' Specifically, it means to generate one way basis functions for 'x' and then all basis functions
 #' up to degree 2 for other variables excluding 'x' in \code{data}. As a result, no interactions will be added for the variable 'x'.
@@ -51,6 +54,7 @@ formula_hal9001 <-
            smoothness_orders = NULL,
            include_zero_order = F,
            bins = NULL,
+           generate_lower_degrees = F,
            exclusive_dot = F) {
     form = formula
     if (is.null(smoothness_orders) | !is.numeric(smoothness_orders)) {
@@ -64,21 +68,59 @@ formula_hal9001 <-
     order_map = smoothness_orders
     form = stringr::str_replace_all(form, " ", "")
 
-    reg = "([^\\s])~([idh]\\(([^\\s()+]+)\\)|\\.(\\^[0-9])?)(?:\\+[ihd]\\(([^\\s()]+)\\))*(\\+\\.(\\^[0-9])?)?$"
+    term_star = stringr::str_match_all(form, "([ihd]\\([^)]+\\)[*][ihd]\\([^+]+\\))")[[1]]
+    term_star = term_star[,-1]
+
+    process_star = function(term){
+      pieces = str_split(term, "[*]")[[1]]
+
+      combs = lapply(1: (length(pieces)-1), function(i){
+        combos = combn(pieces, i)
+        combos = lapply(1:ncol(combos), function(j){combos[,j]})
+
+        return(combos)
+      })
+
+      combs = unlist(combs, recursive = F)
+
+      combs = sapply(combs, function(term){paste0(term, collapse = ":")})
+      return(combs)
+    }
+    if(stringr::str_detect(form, "[*]")){
+      term_star = sapply(term_star, process_star)
+      form = paste0(form, "+", paste0(term_star, collapse = "+"))
+    }
+
+
+
+    form = stringr::str_replace_all(form, "\\)[*:][ihd]\\(", ",")
+
+
+    reg = "([^\\s])~([idh]\\(([^\\s()+]+)\\)|\\.(\\^[0-9])?)(?:[+-][ihd]\\(([^\\s()]+)\\))*(\\+\\.(\\^[0-9])?)?$"
     assertthat::assert_that(stringr::str_detect(form, reg), msg = "Incorrect format for formula.")
     outcome = stringr::str_match(form, "([^\\s]+)~")
+
     outcome = outcome[, 2]
+    if(!(outcome %in% colnames(data))){
+      stop("Outcome not found in data.")
+    }
     X = data[,-which(colnames(data) == outcome), drop = F]
     X_orig = X
     X = quantizer(X, bins)
    Y= data[, outcome]
     names = colnames(X)
 
-    interactions = stringr::str_match_all(form, "[ihd]\\(([^\\s()]+)\\)")[[1]]
+    interactions = stringr::str_match_all(form, "[^-][ihd]\\(([^\\s()]+)\\)")[[1]]
     interactions = interactions[, 2]
 
-    monotone_type = stringr::str_match_all(form, "([[a-z]])\\([^\\s()]+\\)")[[1]]
+    monotone_type = stringr::str_match_all(form, "[^-]([[a-z]])\\([^\\s()]+\\)")[[1]]
     monotone_type = monotone_type[, 2]
+
+    interactions_minus = stringr::str_match_all(form, "[-][ihd]\\(([^\\s()]+)\\)")[[1]]
+    interactions_minus = interactions_minus[, 2]
+
+    monotone_type_minus = stringr::str_match_all(form, "[-]([[a-z]])\\([^\\s()]+\\)")[[1]]
+    monotone_type_minus = monotone_type_minus[, 2]
 
     if (stringr::str_detect(form, "[~+]\\.")) {
 
@@ -91,8 +133,71 @@ formula_hal9001 <-
     }
 
 
+    monotone_type
+
+
+    sub_dots = function(interactions, monotone_type) {
+
+      monotone_type_new = c()
+      inter = lapply(1:length(interactions),   function(i){
+      term = interactions[[i]]
+      cols = unlist(stringr::str_extract_all(term, "[^,]+"))
+
+      index_of_dot = which(cols == ".")
+
+      if(length(index_of_dot)==0){
+        monotone_type_new <<- c(monotone_type_new, monotone_type[i])
+        return(term)
+      }
+      cols_left = setdiff(names,cols[-index_of_dot])
+
+      type = monotone_type[i]
+      monotone_type_new <<- c(monotone_type_new, rep(type, length(cols_left)) )
+      return(stringr::str_replace(term, "\\.", cols_left))
+    })
+      return(list(monotone_type_new, unlist(inter, recursive = F)))
+    }
+    count = 0
+    while(count <=5){
+      if(length(interactions)==0){
+        break
+      }
+      new_inter = sub_dots(interactions, monotone_type)
+      monotone_type= new_inter[[1]]
+      new_inter = new_inter[[2]]
+      if(length(setdiff(interactions, new_inter ))==0){
+
+        break
+      }
+      interactions = new_inter
+
+      count = count +1
+    }
+    count = 0
+    while(count <=5){
+      if(length(interactions_minus)==0){
+        break
+      }
+      new_inter_minus = sub_dots(interactions_minus, monotone_type_minus)
+      monotone_type_minus= new_inter_minus[[1]]
+      new_inter_minus = new_inter_minus[[2]]
+      if(length(setdiff(interactions_minus, new_inter_minus ))==0){
+
+        break
+      }
+      interactions_minus = new_inter_minus
+
+      count = count +1
+    }
+
+
+
+
+
+
     get_index = function(term) {
       cols = unlist(stringr::str_extract_all(term, "[^,]+"))
+
       valid = cols %in% names
       if(any(!valid)){
         stop(paste0("Unknown variable(s) in formula: ", cols[!valid]))
@@ -108,12 +213,38 @@ formula_hal9001 <-
       ind = ind[keep]
       ind = sort(unique(ind))
 
-      return(ind)
+      return((ind))
     }
-    interactions_index = (lapply(interactions, get_index))
+    interactions_index = lapply(interactions, get_index)
     interactions_index = interactions_index[unlist(lapply(interactions_index, function(v) {
       length(v) != 0
     }))]
+
+    interactions_index_minus = lapply(interactions_minus, get_index)
+    interactions_index_minus = interactions_index_minus[unlist(lapply(interactions_index_minus, function(v) {
+      length(v) != 0
+    }))]
+
+
+
+    if(generate_lower_degrees & (length(interactions)!=0)){
+
+
+      monotone_type_new = c()
+      interactions_index_list = lapply(1:length(interactions_index), function(i){
+        term = interactions_index[[i]]
+
+        lst = lapply(1:length(term), function(m){combn(term, m)})
+        result = (unlist(lapply(lst, function(x){lapply(seq_len(ncol(x)), function(i)
+          x[, i])}),recursive = F))
+        monotone_type_new <<- c(monotone_type_new, rep(monotone_type[i], length(result)))
+        return(result)
+      })
+      monotone_type = monotone_type_new
+      interactions_index = unlist(interactions_index_list, recursive = F)
+    }
+
+
 
 
     not_dupes_index = which(!duplicated(interactions_index))
@@ -121,6 +252,7 @@ formula_hal9001 <-
 
     interactions_index = interactions_index[not_dupes_index]
     monotone_type = monotone_type[not_dupes_index]
+
 
 
     if(exclusive_dot){
@@ -132,7 +264,7 @@ formula_hal9001 <-
 
     get_combos = function(deg) {
       set_inds = setdiff(1:length(names),variables_specified)
-      print(set_inds)
+
       if(length(set_inds) == 0){
         return(list())
       }
@@ -157,6 +289,29 @@ formula_hal9001 <-
 
 
     dot_argument_combos = setdiff(all_combinations, interactions_index)
+    dot_argument_combos = setdiff(dot_argument_combos , interactions_index_minus[which(monotone_type_minus == "h")])
+
+    index_to_remove = match(interactions_index_minus,interactions_index)
+
+    if(length(index_to_remove)!=0){
+
+      final_index_to_remove = c()
+      for( i in 1: length(index_to_remove)){
+        other_i = index_to_remove[[i]]
+        if(!is.na(other_i)){
+          if(monotone_type[other_i] == monotone_type_minus[i]){
+            final_index_to_remove = c(final_index_to_remove,other_i)
+          }
+        }
+
+
+      }
+      if(length(final_index_to_remove)!=0){
+        interactions_index = interactions_index[-final_index_to_remove]
+        monotone_type = monotone_type[-final_index_to_remove]
+      }
+
+    }
 
     lower.limits = c()
     upper.limits = c()
@@ -191,6 +346,9 @@ formula_hal9001 <-
       ),
       recursive = F
     )
+    form = stringr::str_replace_all(form, "[+]", " + ")
+    form = stringr::str_replace_all(form, "[~]", " ~ ")
+    form = stringr::str_replace_all(form, "[-]", " - ")
     len = length(basis_listrest)
     upper.limits = c(upper.limits, rep(Inf, len))
     lower.limits = c(lower.limits, rep(-Inf, len))
