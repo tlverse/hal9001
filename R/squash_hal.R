2a62f05f5ebe903ba31b1efcb82f940c46f8d50f
Lars van der Laan <larsvanderlaan@berkeley.edu>
2021-03-17 17:55:45 -0700

    fix squash hal

--- R/squash_hal.R
+++ R/squash_hal.R
@@ -32,7 +32,7 @@ squash_hal_fit <- function(object) {
 
   # find indices for basis functions with non-zero coefficients
   nz_coefs <- which(as.vector(object$coefs)[-1] != 0)
-  new_coefs <- object$coefs[c(1, nz_coefs)]
+  new_coefs <- object$coefs[c(1, nz_coefs +1)]
 
   # extract all basis functions belonging to any group with nonzero coefficient
   nz_basis_groups <- object$copy_map[nz_coefs]
