16aaab00a6349f2d32601a142dd8fe72694ad84c
Haodong Li <haodong_li@berkeley.edu>
2023-02-03 22:00:09 -0800

    fit_uhal function

--- R/hal_undersmooth.R
+++ R/hal_undersmooth.R
@@ -6,36 +6,87 @@
 #'
 
 ###############################################################################
+#'  fit undersmoothed HAL
+#'
+#' @details a wrapper function that execuate undersmoothed HAL and return the fit. See
+#' more parameter definitions in \code{fit_hal}.
+#' @param Nlam A \code{integer} scalar of number of lambda candidates
+
+fit_uhal <- function(X,
+                     Y,
+                     Nlam = 20,
+                     formula = NULL,
+                     X_unpenalized = NULL,
+                     max_degree = ifelse(ncol(X) >= 20, 2, 3),
+                     smoothness_orders = 0,
+                     num_knots = num_knots_generator(
+                       max_degree = max_degree,
+                       smoothness_orders = smoothness_orders,
+                       base_num_knots_0 = 200,
+                       base_num_knots_1 = 50
+                     ),
+                     reduce_basis = 1 / sqrt(length(Y)),
+                     family = c("gaussian", "binomial", "poisson", "cox"),
+                     lambda = NULL,
+                     id = NULL,
+                     offset = NULL,
+                     fit_control = list(
+                       cv_select = TRUE,
+                       n_folds = 10,
+                       foldid = NULL,
+                       use_min = TRUE,
+                       lambda.min.ratio = 1e-4,
+                       prediction_bounds = "default"
+                     ),
+                     basis_list = NULL,
+                     return_lasso = TRUE,
+                     return_x_basis = TRUE,
+                     yolo = FALSE){
+
+  # initialize the undersmoothing procedure
+  args_f <- as.list(environment())
+  args_f <- args_f[which(names(args_f) != 'Nlam')]
+  hal_init <- do.call(undersmooth_init,
+                      args = args_f)
+
+  # do undersmoothed HAL
+  hal_under <- undersmooth_hal(X = X,
+                               Y = Y,
+                               fit_init = hal_init$fit_init,
+                               basis_mat = hal_init$basis_mat,
+                               Nlam = Nlam,
+                               family = family)
+
+  print(paste0("Initial CV Lambda: ", hal_under$lambda_init))
+
+  # return undersmoothed HAL
+  basis_mat_full <- hal_init$fit_init$x_basis
+  lambda_under <- hal_under$lambda_under
+
+  uhal_fit <- glmnet(basis_mat_full,
+                     Y,
+                     lambda=lambda_under,
+                     family = family,
+                     standardize = FALSE)
+
+  return(uhal_fit)
+}
+
+###############################################################################
 #' initialize undersmoothed HAL
 #'
 #' @details fit a regular 0-order HAL(\code{smoothness_orders = 0}) with binning
 #'  (\code{num_knots}), select the set of basis functions with non-zero coefs
 #'  which will be used in following procedure. Note this is not the only valid initializing
 #'  procedure, the user can define their own initialization.
-#' @param X An input \code{matrix} with dimensions number of observations -by-
-#'  number of covariates that will be used to derive the design matrix of basis
-#'  functions.
-#' @param Y A \code{numeric} vector of observations of the outcome variable.
-#' @param family A \code{character} or a \code{\link[stats]{family}} object
-#'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
-#'  family for a generalized linear model. See more details in \code{fit_hal}.
+#' @keywords internal
 
-undersmooth_init <- function(X, Y, family = "gaussian"){
-
-  n <- length(Y)
+undersmooth_init <- function(X, Y, ...) {
 
   # get initial fit
-  fit_init <- fit_hal(X = X,
-                      Y = Y,
-                      smoothness_orders = 0,
-                      return_x_basis = TRUE,
-                      family = family,
-                      num_knots = num_knots_generator(
-                                   max_degree = ifelse(ncol(X) >= 20, 2, 3),
-                                   smoothness_orders = 0,
-                                   base_num_knots_0 = max(100, ceiling(sqrt(n)))
-                                 )
-                      )
+  args_f <- c(as.list(environment()), list(...))
+  args_f <- args_f[which(names(args_f) != 'Nlam')]
+  fit_init <- do.call(fit_hal, args = args_f)
 
   # select the non-zero directions/basis
   init_coef <-fit_init$coefs[-1]
@@ -66,12 +117,12 @@ undersmooth_init <- function(X, Y, family = "gaussian"){
 #' @param family A \code{character} or a \code{\link[stats]{family}} object
 #'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
 #'  family for a generalized linear model. See more details in \code{fit_hal}.
+#' @keywords internal
 
 undersmooth_hal <- function(X,
                             Y,
                             fit_init,
                             basis_mat,
-                            criterion = NULL,
                             Nlam = 20,
                             family = "gaussian"){
   n <- length(Y)
@@ -94,16 +145,13 @@ undersmooth_hal <- function(X,
   resid_mat <- pred_mat - Y
 
   # check the criterion (global)
-  # TBD user-specified criterion (e.g. target parameter driven)
-  if (is.null(criterion)){
-    max_score <- get_maxscore(basis_mat = basis_mat,
-                              resid_mat = resid_mat,
-                              sd_est = sd_est,
-                              Nlam = Nlam, us_fit = us_fit)
+  max_score <- get_maxscore(basis_mat = basis_mat,
+                            resid_mat = resid_mat,
+                            sd_est = sd_est,
+                            Nlam = Nlam, us_fit = us_fit)
 
-    # get the first lambda that satisfies the criteria
-    lambda_under <- us_lambda[max_score <= 1/(sqrt(n)*log(n))][1]
-  }
+  # get the first lambda that satisfies the criteria
+  lambda_under <- us_lambda[max_score <= 1/(sqrt(n)*log(n))][1]
 
   # collect results
   coef_mat <- as.matrix(us_fit$beta)
@@ -134,6 +182,7 @@ undersmooth_hal <- function(X,
 #' @param sd_est A numeric vector containing the sd of each column of \code{basis_mat}.
 #' @param Nlam Number of lambda candidates.
 #' @param us_fit The \code{glmnet} fit of the sequence of candidate lambdas.
+#' @keywords internal
 
 get_maxscore <- function(basis_mat, resid_mat, sd_est, Nlam, us_fit){
 
@@ -147,6 +196,36 @@ get_maxscore <- function(basis_mat, resid_mat, sd_est, Nlam, us_fit){
   return(max_score)
 }
 
+###############################################################################
+
+#' A default generator for the \code{num_knots} argument for each degree of
+#' interactions and the smoothness orders.
+#'
+#' @param d interaction degree.
+#' @param smoothness_orders see \code{\link{fit_hal}}.
+#' @param base_num_knots_0 The base number of knots for zeroth-order smoothness
+#'  basis functions. The number of knots by degree interaction decays as
+#'  `base_num_knots_0/2^(d-1)` where `d` is the interaction degree of the basis
+#'  function.
+#' @param base_num_knots_1 The base number of knots for 1 or greater order
+#'  smoothness basis functions. The number of knots by degree interaction
+#'  decays as `base_num_knots_1/2^(d-1)` where `d` is the interaction degree of
+#'  the basis function.
+#'
+#' @keywords internal
+
+num_knots_generator <- function(max_degree, smoothness_orders, base_num_knots_0 = 500,
+                                base_num_knots_1 = 200) {
+  if (all(smoothness_orders > 0)) {
+    return(sapply(seq_len(max_degree), function(d) {
+      round(base_num_knots_1 / 2^(d - 1))
+    }))
+  } else {
+    return(sapply(seq_len(max_degree), function(d) {
+      round(base_num_knots_0 / 2^(d - 1))
+    }))
+  }
+}
 
 
 
--- tests/testthat/test-undersmooth.R
+++ tests/testthat/test-undersmooth.R
@@ -1,8 +1,11 @@
 context("Unit test for procedures relating to undersmooth HAL.")
 # library(microbenchmark)
+library(hal9001)
+library(glmnet)
+library(here)
 
+source(paste0(here(), "/R/hal_undersmooth.R"))
 
-# library(here)
 # library(devtools)
 # load_all(here())
 
@@ -14,18 +17,49 @@ p <- 3
 x <- matrix(rnorm(n * p), n, p)
 y <- x[, 1] * sin(x[, 2]) + rnorm(n, mean = 0, sd = 0.2)
 
-# initialize the undersmoothing procedure
-hal_init <- undersmooth_init(X=x, Y=y, family = "gaussian")
+# fit undersmoothed HAL
+uhal_fit <- fit_uhal(X=x, Y=y, family="gaussian")
+uhal_fit$lambda
 
-# do undersmoothed HAL
-hal_under <- undersmooth_hal(X=x,
-                             Y=y,
-                             fit_init=hal_init$fit_init,
-                             basis_mat=hal_init$basis_mat,
-                             Nlam = 20,
-                             family = "gaussian")
-
-hal_under$lambda_init
-hal_under$lambda_under
-hal_under$spec_under
-
+# # initialize the undersmoothing procedure
+# hal_init <- undersmooth_init(X=x, Y=y,
+#                              Nlam = 20,
+#                              formula = NULL,
+#                              X_unpenalized = NULL,
+#                              max_degree = ifelse(ncol(x) >= 20, 2, 3),
+#                              smoothness_orders = 0,
+#                              num_knots = num_knots_generator(
+#                                max_degree = ifelse(ncol(x) >= 20, 2, 3),
+#                                smoothness_orders = 0,
+#                                base_num_knots_0 = 200,
+#                                base_num_knots_1 = 50
+#                              ),
+#                              reduce_basis = 1 / sqrt(length(y)),
+#                              family = c("gaussian", "binomial", "poisson", "cox"),
+#                              lambda = NULL,
+#                              id = NULL,
+#                              offset = NULL,
+#                              fit_control = list(
+#                                cv_select = TRUE,
+#                                n_folds = 10,
+#                                foldid = NULL,
+#                                use_min = TRUE,
+#                                lambda.min.ratio = 1e-4,
+#                                prediction_bounds = "default"
+#                              ),
+#                              basis_list = NULL,
+#                              return_lasso = TRUE,
+#                              return_x_basis = TRUE,
+#                              yolo = FALSE)
+#
+# # do undersmoothed HAL
+# hal_under <- undersmooth_hal(X=x,
+#                              Y=y,
+#                              fit_init=hal_init$fit_init,
+#                              basis_mat=hal_init$basis_mat,
+#                              Nlam = 20,
+#                              family = "gaussian")
+#
+# hal_under$lambda_init
+# hal_under$lambda_under
+# hal_under$spec_under
